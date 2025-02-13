0034f2c9eb1cb0340ca00138478d4dfa0351fccb
Murillo-dev551 <murillooliver87@gmail.com>
2025-02-13 20:43:40 -0300

    Senhas2.py

--- Pasta para projetos github/Senhas2.py
+++
@@ -1,8 +0,0 @@
-senha = int(input("Digite a senha: "))
-
-while senha != 2002:
-    print("Senha incorreta. Tente na próxima.")
-    senha = int(input("Digite a senha: "))
-    if senha == 2002:
-        print("Senha correta.")
-        break
