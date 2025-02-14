c3bc92c27afcb937145bb149287f91583133429f
Murillo-dev551 <murillooliver87@gmail.com>
2025-02-14 15:48:36 -0300

    Create contador de numeros impares.java
    Conta os números impares em java, daqui a pouco faço um em python

---
+++ Pasta para projetos github/java/contador de numeros impares.java
@@ -0,0 +1,24 @@
+package curso_programacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class teste3 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		
+		for (int i=0; i<x; i++) {
+			if (i % 2 == 1) {
+				System.out.println(i);
+			}
+
+		}
+		sc.close();
+		
+	}
+}
